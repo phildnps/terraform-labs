d15fd3afb0961dbe8ee891979ece503bd3d75391
phildnps <60966999+phildnps@users.noreply.github.com>
2020-02-14 11:50:06 +0000

    Delete provider.tf

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-  subscription_id = "6a0226de-bc52-45e1-bfd4-34537ff4db16"
-  client_id       = "7ce04be9-2210-4172-8248-52990d0cdef3"
-  client_secret   = "1a4dc617-c801-446f-ac17-d63a7a887778"
-  tenant_id       = "9060f9a0-d46c-4ec4-a299-40398404c323"
-}
