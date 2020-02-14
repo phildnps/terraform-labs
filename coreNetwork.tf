9bf358ddf3a149f9b84c2762b0a72bc1044686e3
Phil Dunkerley <phil.dunkerley@northgateps.com>
2020-02-14 09:21:36 +0000

    End of lab 3

---
+++ coreNetwork.tf
@@ -0,0 +1,63 @@
+resource "azurerm_resource_group" "core" {
+   name = "core"
+   location = var.loc
+   tags = var.tags
+}
+
+resource "azurerm_public_ip" "vpnGatewayPublicIp" {
+   name = "vpnGatewayPublicIp"
+   resource_group_name = "${azurerm_resource_group.core.name}"
+   allocation_method = "Dynamic"
+   location = azurerm_resource_group.core.location
+}
+
+resource "azurerm_virtual_network" "core" {
+   name = "core"
+   location = azurerm_resource_group.core.location
+   resource_group_name = "${azurerm_resource_group.core.name}"
+   address_space = ["10.0.0.0/16"]
+   dns_servers = ["1.1.1.1", "1.0.0.1"]
+}
+
+resource "azurerm_subnet" "GatewaySubnet" {
+   name = "GatewaySubnet"
+   resource_group_name = azurerm_resource_group.core.name
+   virtual_network_name = azurerm_virtual_network.core.name
+   address_prefix = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "training" {
+   name = "training"
+   resource_group_name = azurerm_resource_group.core.name
+   virtual_network_name = azurerm_virtual_network.core.name
+   address_prefix = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "dev" {
+   name = "dev"
+   resource_group_name = azurerm_resource_group.core.name
+   virtual_network_name = azurerm_virtual_network.core.name
+   address_prefix = "10.0.2.0/24"
+}
+
+/*
+resource "azurerm_virtual_network_gateway" "vpnGateway" {
+  name                = "vpnGateway"
+  location            = azurerm_resource_group.core.location
+  resource_group_name = azurerm_resource_group.core.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = true
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vpnGwConfig1"
+    public_ip_address_id          = azurerm_public_ip.vpnGatewayPublicIp.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.GatewaySubnet.id
+  }
+}
+*/
