2f4acd3ac849a92e8cb7101b13290c86e1349cfd
Phil Dunkerley <phil.dunkerley@northgateps.com>
2020-02-14 12:07:56 +0000

    Corrected gitignore

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   subscription_id = "6a0226de-bc52-45e1-bfd4-34537ff4db16"
-  client_id       = "7ce04be9-2210-4172-8248-52990d0cdef3"
-  client_secret   = "1a4dc617-c801-446f-ac17-d63a7a887778"
+  client_id       = "43d02f16-a09e-4b77-aec3-b19c7d4a4b68"
+  client_secret   = "19c6e431-f677-4bd8-9210-e9c1d81100c4"
   tenant_id       = "9060f9a0-d46c-4ec4-a299-40398404c323"
 }
